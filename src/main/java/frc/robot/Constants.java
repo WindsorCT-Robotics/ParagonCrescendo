10f4f678eb2e8c461604de0d5da55c9b2ab6c6fc
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2024-02-24 11:18:41 -0500

    Refactor outtake constants with rpm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,5 @@
 package frc.robot;
 
 public class Constants {
-
-    public static final int OUTTAKE_MOTOR_MAX_RPM = 11000;
-    public static final double OUTTAKE_MOTOR_GEAR_RATIO = (double) 1 / 16;
-    public static final double OUTTAKE_MOTOR_TARGET_PERCENT = INTAKE_ROLLERS_TARGET_RPM / (OUTTAKE_MOTOR_MAX_RPM * OUTTAKE_MOTOR_GEAR_RATIO);
-
-    public static final double OUTTAKE_ADJUST_DISTANCE = 2.5; // Inches
-    public static final int OUTTAKE_ADJUST_DIRECTION = -1; // Too far is -1, not far enough is 1.
-    public static final double OUTTAKE_ROLLERS_CIRCUMFERENCE = Math.PI * 1.67; // Outer diameter is 1.67"
-    public static final double OUTTAKE_ADJUST_ROTATIONS = OUTTAKE_ADJUST_DISTANCE / (OUTTAKE_MOTOR_GEAR_RATIO * OUTTAKE_ROLLERS_CIRCUMFERENCE);
-    public static final double OUTTAKE_ADJUST_SPEED = OUTTAKE_ADJUST_DIRECTION * 0.2;
 }
 
