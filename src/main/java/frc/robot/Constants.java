fe8786485d62d15c4cd0d5689414305645051387
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2024-02-24 11:18:21 -0500

    Refactor intake constants with rpm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,6 @@
 package frc.robot;
 
 public class Constants {
-    public static final int INTAKE_ROLLERS_TARGET_RPM = 500;
-    public static final int INTAKE_MOTOR_MAX_RPM = 5600;
-    public static final double INTAKE_MOTOR_GEAR_RATIO = (double) 14 / 48;
-    public static final double INTAKE_MOTOR_TARGET_PERCENT = INTAKE_ROLLERS_TARGET_RPM / (INTAKE_MOTOR_MAX_RPM * INTAKE_MOTOR_GEAR_RATIO);
 
     public static final int OUTTAKE_MOTOR_MAX_RPM = 11000;
     public static final double OUTTAKE_MOTOR_GEAR_RATIO = (double) 1 / 16;
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,11 +1,12 @@
 package frc.robot.subsystems;
-import static frc.robot.Constants.*;
 
 import com.revrobotics.CANSparkMax;
+
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Units.Percent;
+import frc.robot.Units.RotationsPerMinute;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,17 +18,30 @@ public class IntakeSubsystem extends SubsystemBase {
     private final CANSparkMax rollers;
     private final DigitalInput beamBreaker;
 
+    private static final int MOTOR_MAX_RPM = 5600;
+    private static final double MOTOR_GEAR_RATIO = (double) 14 / 48;
+    public static RotationsPerMinute targetRPM = new RotationsPerMinute(500, 1);
+
     public IntakeSubsystem() {
-        rollers = new CANSparkMax(INTAKE_ROLLER_MOTOR_CANID, MotorType.kBrushless);
-        beamBreaker = new DigitalInput(INTAKE_BEAM_BREAKER_PIN);
+        rollers = new CANSparkMax(ROLLER_MOTOR_CANID, MotorType.kBrushless);
+        beamBreaker = new DigitalInput(BEAM_BREAKER_PIN);
+
+        SmartDashboard.putNumber("Intake Target RPM", targetRPM.asDouble());
     }
 
     @Override
     public void periodic() {
        SmartDashboard.putBoolean("Intake Beam Breaker ", beamBreaker.get());
+
+       targetRPM = new RotationsPerMinute(SmartDashboard.getNumber("Intake Target RPM", targetRPM.asDouble()), 1);
     }
 
     public void moveRollers(Percent speed) {
+        rollers.set(speed.asDouble());
+    }
+
+    public void intakeRollers() {
+        Percent speed = new Percent(targetRPM.asDouble() / (MOTOR_MAX_RPM * MOTOR_GEAR_RATIO));
         rollers.set(speed.asDouble());
     }
 
