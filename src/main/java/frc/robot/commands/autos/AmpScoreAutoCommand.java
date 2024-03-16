59c45066e76cbc5a891e2b4b5a134a9d1c650338
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2024-03-16 15:23:27 -0400

    Update AmpScoreAutoCommand.java

--- src/main/java/frc/robot/commands/autos/AmpScoreAutoCommand.java
+++ src/main/java/frc/robot/commands/autos/AmpScoreAutoCommand.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
@@ -19,17 +18,36 @@ import frc.robot.commands.AmpScoreCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.OuttakeSubsystem;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import java.util.Optional;
 
 public class AmpScoreAutoCommand extends SequentialCommandGroup {
     DriveSubsystem drive;
     ArmSubsystem arm;
     OuttakeSubsystem outtake;
+    
+    public enum AllianceColor {
+        BLUE,
+        RED
+    }
 
-    public AmpScoreAutoCommand(DriveSubsystem drive, ArmSubsystem arm, OuttakeSubsystem outtake) {
+    AllianceColor color;
+
+    public AmpScoreAutoCommand(DriveSubsystem drive, ArmSubsystem arm, OuttakeSubsystem outtake, Optional<Alliance> alliance) {
         addRequirements(drive);
         this.drive = drive;
         this.arm = arm;
         this.outtake = outtake;
+
+        if (alliance.isPresent()) {
+            if(alliance.get() == Alliance.Blue) {
+                color = AllianceColor.BLUE;
+            } else {
+                color = AllianceColor.RED;
+            }
+        } else {
+            color = AllianceColor.BLUE;
+        }
 
         var autoVoltageConstraint =
         new DifferentialDriveVoltageConstraint(
@@ -52,7 +70,9 @@ public class AmpScoreAutoCommand extends SequentialCommandGroup {
                 .setReversed(true);
 
         // An example trajectory to follow. All units in meters.
-        Trajectory initialDriveTrajectory =
+        Trajectory initialDriveTrajectory;
+        if (color == AllianceColor.BLUE) {
+            initialDriveTrajectory =
             TrajectoryGenerator.generateTrajectory(
                 // Start at the origin facing the +X direction
                 new Pose2d(0, 0, new Rotation2d(Math.PI)),
@@ -62,6 +82,18 @@ public class AmpScoreAutoCommand extends SequentialCommandGroup {
                 new Pose2d(1.23, 0.5, new Rotation2d(282*Math.PI/180)), //-0.508, -0.451
                 // Pass config
                 config);
+        } else {
+            initialDriveTrajectory =
+            TrajectoryGenerator.generateTrajectory(
+                // Start at the origin facing the +X direction
+                new Pose2d(0, 0, new Rotation2d(Math.PI)),
+                // Pass through these two interior waypoints, making an 's' curve path
+                List.of(),
+                // End 3 meters straight ahead of where we started, facing forward
+                new Pose2d(1.23, -0.5, new Rotation2d(78*Math.PI/180)), //-0.508, -0.451
+                // Pass config
+                config);
+        }
 
         RamseteCommand initialDriveCommand =
             new RamseteCommand(
